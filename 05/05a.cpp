f22287d70ca9292c8ee0973c2efb0295d7a12d9b
Steve Hollasch <steve@hollasch.net>
2024-01-16 14:12:54 -0800

    Solved 05a

--- 05/05a.cpp
+++ 05/05a.cpp
@@ -134,17 +134,244 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+using Value = unsigned long;
+
+
+const bool debug = false;
+
+
+struct Mapping {
+    // A single source-to-destination mapping
+
+    Value destinationRangeStart;
+    Value sourceRangeStart;
+    Value rangeLength;
+
+    Value evaluate(Value source) const {
+        if (sourceRangeStart <= source && source < sourceRangeStart + rangeLength)
+            return source - sourceRangeStart + destinationRangeStart;
+        return source;
+    }
+
+    bool read() {
+        string line;
+        getline(cin, line);
+        if (line.empty()) return false;
+        sscanf(line.c_str(), "%lu %lu %lu", &destinationRangeStart, &sourceRangeStart, &rangeLength);
+        return true;
+    }
+
+    void dump() const {
+        cout << destinationRangeStart << " " << sourceRangeStart << " " << rangeLength << '\n';
+    }
+};
+
+
+Value map(const vector<Mapping>& mappings, Value x) {
+    // Given a set of mappings, evaluates each in order to map from the source x to its destination.
+    for (auto& mapping : mappings) {
+        Value y = mapping.evaluate(x);
+        if (y != x) return y;
+    }
+    return x;
+}
+
+
+struct Mappings {
+    // The full set of garden mappings
+
+    vector<Mapping> seedToSoil;
+    vector<Mapping> soilToFertilizer;
+    vector<Mapping> fertilizerToWater;
+    vector<Mapping> waterToLight;
+    vector<Mapping> lightToTemperature;
+    vector<Mapping> temperatureToHumidity;
+    vector<Mapping> humidityToLocation;
+
+    Value location(Value humidity) const {
+        return map(humidityToLocation, humidity);
+    }
+
+    Value humidity(Value temperature) const {
+        return map(temperatureToHumidity, temperature);
+    }
+
+    Value temperature(Value light) const {
+        return map(lightToTemperature, light);
+    }
+
+    Value light(Value water) const {
+        return map(waterToLight, water);
+    }
+
+    Value water(Value fertilizer) const {
+        return map(fertilizerToWater, fertilizer);
+    }
+
+    Value fertilizer(Value soil) const {
+        return map(soilToFertilizer, soil);
+    }
+
+    Value soil(Value seed) const {
+        return map(seedToSoil, seed);
+    }
+
+    void dump() const {
+        cout << "\nseed-to-soil map:\n";
+        for (auto mapping : seedToSoil) mapping.dump();
+
+        cout << "\nsoil-to-fertilizer map:\n";
+        for (auto mapping : soilToFertilizer) mapping.dump();
+
+        cout << "\nfertilizer-to-water map:\n";
+        for (auto mapping : fertilizerToWater) mapping.dump();
+
+        cout << "\nwater-to-light map:\n";
+        for (auto mapping : waterToLight) mapping.dump();
+
+        cout << "\nlight-to-temperature map:\n";
+        for (auto mapping : lightToTemperature) mapping.dump();
+
+        cout << "\ntemperature-to-humidity map:\n";
+        for (auto mapping : temperatureToHumidity) mapping.dump();
+
+        cout << "\nhumidity-to-location map:\n";
+        for (auto mapping : humidityToLocation) mapping.dump();
+    }
+};
+
+
+struct GardenData {
+    // The full set of garden data
+    vector<Value> seeds;
+    Mappings    mappings;
+
+    void dump() const {
+        cout << "seeds:";
+        for (auto seed : seeds)
+            cout << ' ' << seed;
+        cout << '\n';
+
+        mappings.dump();
+    }
+
+    Value nearestLocation() const {
+        // Return the nearest location (smallest value) of all seeds.
+
+        if (debug) cout << '\n';
+        bool first = true;
+        Value nearest = 0;
+
+        for (auto seed : seeds) {
+            Value soil = mappings.soil(seed);
+            Value fertilizer = mappings.fertilizer(soil);
+            Value water = mappings.water(fertilizer);
+            Value light = mappings.light(water);
+            Value temperature = mappings.temperature(light);
+            Value humidity = mappings.humidity(temperature);
+            Value location = mappings.location(humidity);
+
+            if (debug) {
+                cout << "Seed " << seed
+                     << ", soil " << soil
+                     << ", fertilizer " << fertilizer
+                     << ", water " << water
+                     << ", light " << light
+                     << ", temperature " << temperature
+                     << ", humidity " << humidity
+                     << ", location " << location
+                     << '\n';
+            }
+
+            if (first || location < nearest) {
+                nearest = location;
+                first = false;
+            }
+        }
+
+        if (debug) cout << '\n';
+
+        return nearest;
+    }
+};
+
+
+void readSeeds(const string& line, GardenData& data) {
+    // Read the list of seeds from the input line.
+
+    data.seeds.clear();
+
+    auto *ptr = line.c_str();
+    while (ptr) {
+        while (ptr && *ptr && *ptr != ' ') ++ptr;  // Scan to next space.
+        if (!ptr || !*ptr) break;
+        ++ptr;  // Skip the space.
+        data.seeds.push_back(atoi(ptr));
+    }
+}
+
+
+void readMappings(vector<Mapping>& mappings) {
+    // Read a set of mappings from standard input.
+
+    string line;
+    Mapping mapping;
+    while (mapping.read())
+        mappings.push_back(mapping);
+}
+
+
+bool readSection(GardenData& data) {
+    // Read a section of the input file, which is a set of mappings from one category to another.
+    // Each line in the section is a mapping, and each mapping is three integers separated by
+    // whitespace: the destination range start, the source range start, and the range length.
+
+    string line;
+
+    while (true) {
+        if (!getline(cin, line)) return false;
+        if (!line.empty()) break;
+    }
+
+    if (line.starts_with("seeds: "))
+        readSeeds(line, data);
+    else if (line == "seed-to-soil map:")
+        readMappings(data.mappings.seedToSoil);
+    else if (line == "soil-to-fertilizer map:")
+        readMappings(data.mappings.soilToFertilizer);
+    else if (line == "fertilizer-to-water map:")
+        readMappings(data.mappings.fertilizerToWater);
+    else if (line == "water-to-light map:")
+        readMappings(data.mappings.waterToLight);
+    else if (line == "light-to-temperature map:")
+        readMappings(data.mappings.lightToTemperature);
+    else if (line == "temperature-to-humidity map:")
+        readMappings(data.mappings.temperatureToHumidity);
+    else if (line == "humidity-to-location map:")
+        readMappings(data.mappings.humidityToLocation);
+    else {
+        cout << "Unknown line: " << line << '\n';
+        return false;
+    }
+
+    return true;
+}
 
 
 int main() {
-    string line;
-    int sum = 0;
+    GardenData gardenData;
 
-    while (getline(cin, line)) {
-    }
+    while (readSection(gardenData))
+        continue;
 
-    cout << sum << '\n';
+    if (debug) gardenData.dump();
+
+    Value nearestLocation = gardenData.nearestLocation();
+
+    cout << "Nearest location: " << nearestLocation << '\n';
+
     return 0;
 }
